bcd8e4390029e96c9fd29eca5b582471bc60f520
Ben Hardill <hardillb@gmail.com>
2016-07-20 10:02:49 +0100

    adds license and copywrite statement to files

--- tinkerforge.js
+++ tinkerforge.js
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+
 "use strict";
 
 
@@ -628,8 +645,12 @@ module.exports = function(RED){
 
         node.on('input', function(msg){
             if(node.ao) {
-                var v = Math.round(msg.payload * 1000);
-                node.ao.setOutputVoltage(v);
+                if (typeof msg.payload === 'number') {
+                    if (msg.payload >=0 && msg.parts <= 12) {
+                        var v = Math.round(msg.payload * 1000);
+                        node.ao.setOutputVoltage(v);
+                    }
+                }
             }
         });
         
