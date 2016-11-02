3c0a6bf800808d34ffd62efe1e78d0cd4226a7be
chris <info@cu-tec.de>
2016-11-02 18:08:44 +0000

    Update RemoteSwitch.js

--- bricklets/RemoteSwitch.js
+++ bricklets/RemoteSwitch.js
@@ -56,7 +56,7 @@ module.exports = function(RED) {
  		    }
                 switch (node.mode) {
                 case 'A':node.md.switchSocketA(node.addr1, node.addr2, switchto);break;
-                case 'B':node.md.switchSocketB(node.addr1, node.addr2, dwitchto);break;
+                case 'B':node.md.switchSocketB(node.addr1, node.addr2, switchto);break;
                 }
 	      }
 	    }         
